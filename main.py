cb2350e37855cf1f49033526fc4acc3937cfa5cc
RekaOne <amaury.jacobe1@gmail.com>
2026-04-24 11:49:59 +0200

    Init the logging feature

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 """ Module main.py pour l'application CAF avec
     tkinter en MVC """
 import tkinter as tk
+import logging # imported bib logging
 from modelcaf import ModelCaf
 from viewcaf import ViewCaf
 from controllercaf import ControllerCaf
 
 
+logger = logging.getLogger("logCaf") # add a logger
+logging.basicConfig(
+    filename="logCaf.log",
+    format="%(asctime)s %(message)s",
+    level=logging.INFO)
+
 def main():
+    logging.info("Demarrage")
     v = ViewCaf()
     m = ModelCaf()
     c = ControllerCaf(model=m, view=v)
